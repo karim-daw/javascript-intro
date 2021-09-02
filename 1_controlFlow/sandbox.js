5aa2e3a8a20af315ff6afab001f8ff8409cdb8e5
karimdaw1991 <karimdaw91@gmail.com>
2021-09-02 23:46:48 +0200

    finish control flow

--- 1_controlFlow/sandbox.js
+++ 1_controlFlow/sandbox.js
@@ -7,7 +7,6 @@
 // console.log('loop finished');
 
 const names = ['sean','mario','luigi'];
-
 for(let i = 0; i < names.length; i++)
 {
    //console.log(names[i] + " " + i)
@@ -17,9 +16,7 @@ for(let i = 0; i < names.length; i++)
    console.log(html);
 }
 
-
 // while loops
-
 let i = 5
 while(i < names.length)
 {
@@ -28,7 +25,6 @@ while(i < names.length)
 }
 
 // do while loops, execuse block a code whether condition is true or not first
-
 do
 {
    console.log('val of i is: ', i);
@@ -53,7 +49,7 @@ console.log("that is a lot of ninjas");
 // if else statments
 // logical operators - OR || and AND &&
 
-const password = 'password'
+const password = 'p@ssword'
 if(password.length >= 8 && password.includes('@'))
 {
    console.log('password is long enough');
@@ -64,6 +60,54 @@ else if(password.length >= 4 || password.includes('@'))
 }
 else
 {
-   console.log('password is too short')
+   console.log('password is too short');
 }
+
+// logical NOT (!)
+let user = false;
+
+if(!user)
+{
+   console.log("user aint there");
+}
+else{console.log("he/she is there")};
+
+
+// break and continue 
+const scores = [50,60,70,32,20,0,100,500,600];
+
+for(let i = 0; i < scores.length; i++)
+{
+   console.log('your score: ', scores[i]);
+   if(scores[i] === 100)
+   {
+      console.log('congrates, you got the top score!');
+      break
+   }
+}
+
+// switch statments - good for evalauting variables for several different cases
+const grade = 'D';
+
+switch(grade)
+{
+   case 'A':
+      console.log('you got an A!');
+      break
+   case 'B':
+      console.log('you got an B!');
+      break
+   case 'C':
+      console.log('you got an C!');
+      break
+   case 'D':
+      console.log('you got an D!');
+      break
+   case 'E':
+      console.log('you got an E!');
+      break
+      
+}
+
+// using if statements
 
