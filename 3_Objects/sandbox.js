16000540da2755c893fe928b3d248f12322c3914
karimdaw1991 <karimdaw91@gmail.com>
2021-09-07 22:54:36 +0200

    use math object

--- 3_Objects/sandbox.js
+++ 3_Objects/sandbox.js
@@ -1,12 +1,26 @@
 // Object literals
 
+
+// creating an array of objects
+const blogs =[
+   { title: 'why mac and cheese rules', likes: 30 },
+   { title: ' ten things to make with marmite', likes: 50 }
+];
+
+// console.log(blogs)
+
+
+
 // this is a key value pair, 
 let user = {
    name: 'crystal',
    age: 30,
    email: 'karimdaw91@gmail.com',
    location: 'berlin',
-   blogs: ['why mac and cheese rules', '10 things to make with marmite'],
+   blogs: [
+      { title: 'why mac and cheese rules', likes: 30 },
+      { title: ' ten things to make with marmite', likes: 50 }
+   ],
    login: function(){
       console.log('user logged in');
    },
@@ -18,7 +32,7 @@ let user = {
       console.log('this user has written the following blogs:');
       // can use foreach here
       this.blogs.forEach(function(blog) {
-         console.log(blog);
+         console.log(blog.title, blog.likes);
       })
    },
    logEnd(){ // this is a different way to write methods inside objects
@@ -28,22 +42,36 @@ let user = {
 
 // calling method in user object
 user.login();
-user.logout();
 user.logBlogs();
-user.logEnd();
+user.logout();
+// user.logEnd();
 
-// testing outputting properties
-console.log(user.age)
-console.log(user.name)
+// // testing outputting properties
+// console.log(user.age)
+// console.log(user.name)
 
-// can update property
-user.age = 35;
-console.log(user.age)
+// // can update property
+// user.age = 35;
+// console.log(user.age)
 
-// can also use sqaure brackets
-console.log(user['location']);
-user['name'] = 'chun -li' // helpfull if you wanted to make a variable
-console.log(user.name)
-console.log(typeof user) // its of object type
+// // can also use sqaure brackets
+// console.log(user['location']);
+// user['name'] = 'chun -li' // helpfull if you wanted to make a variable
+// console.log(user.name)
+// console.log(typeof user) // its of object type
 
+// using Math object
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
+const area = 7.731;
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.round(area));
+console.log(Math.trunc(area)); // takes just the first integer
+
+// generate random numbers
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100)) // nice way to get random number 0 - 100
