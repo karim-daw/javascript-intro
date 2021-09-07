b7b22e3cf6b67e472ce3f934eee30f6346924812
karimdaw1991 <karimdaw91@gmail.com>
2021-09-07 22:43:32 +0200

    created examples of object litereals

--- 3_Objects/sandbox.js
+++ 3_Objects/sandbox.js
@@ -6,9 +6,33 @@ let user = {
    age: 30,
    email: 'karimdaw91@gmail.com',
    location: 'berlin',
-   blogs: ['why mac and cheese rules', '10 things to make with marmite']
+   blogs: ['why mac and cheese rules', '10 things to make with marmite'],
+   login: function(){
+      console.log('user logged in');
+   },
+   logout: function(){
+      console.log('user logged out');
+   },
+   logBlogs: function(){ // arrow functions would not work
+ 
+      console.log('this user has written the following blogs:');
+      // can use foreach here
+      this.blogs.forEach(function(blog) {
+         console.log(blog);
+      })
+   },
+   logEnd(){ // this is a different way to write methods inside objects
+      console.log('this is a different way to write regular funcitons as methods')
+   }
 };
 
+// calling method in user object
+user.login();
+user.logout();
+user.logBlogs();
+user.logEnd();
+
+// testing outputting properties
 console.log(user.age)
 console.log(user.name)
 
@@ -21,4 +45,5 @@ console.log(user['location']);
 user['name'] = 'chun -li' // helpfull if you wanted to make a variable
 console.log(user.name)
 console.log(typeof user) // its of object type
+
 
