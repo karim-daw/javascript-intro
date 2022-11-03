05cf6f6207aa26b5934d68d19b61095fc391c9ce
karimdaw1991 <karimdaw91@gmail.com>
2022-11-03 23:14:15 +0100

    es6 java script way of writing classes

--- 5_Classes/sandbox.js
+++ 5_Classes/sandbox.js
@@ -1,28 +1,51 @@
-class Human{
-   constructor () {
-      this.gender = 'male'
-   }
+// these are the more tradiational ways of writing class
 
-   printGender () {
+// class Human{
+//    constructor () {
+//       this.gender = 'male'
+//    }
+
+//    printGender () {
+//       console.log(this.gender)
+//    }
+
+// }
+
+// class Person extends Human {
+//    constructor() {
+//       super()
+//       this.name = 'Max'
+//    }
+//       printGender () {
+//       console.log(this.gender)
+//    }
+//       printName () {
+//       console.log(this.name)
+//    }
+// }
+
+
+// this is the more modern way of writing classes
+class Human{
+      gender = 'male'
+
+   printGender = () => {
       console.log(this.gender)
    }
-
 }
-
 
 class Person extends Human {
-   constructor() {
-      super()
-      this.name = 'Max'
-   }
-      printGender () {
-      console.log(this.gender)
-   }
-      printName () {
-      console.log(this.name)
+      name = 'Max'
+   
+   printName = () => {
+      console.log( this.name )
    }
 }
+
+
+
 
 const person = new Person()
 person.printGender()
 person.printName()
+
